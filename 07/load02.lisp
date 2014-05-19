04a9e8bb91496664bb593ceeebc03911986ff77e
honz4 <j4n.v1cek@gmail.com>
2014-05-19 16:00:02 -0700

    pridan .gitignore a u/opraveno kresleni segmentove cislice

--- 07/load02.lisp
+++ 07/load02.lisp
@@ -4,6 +4,14 @@
 (load "../micro-graphics/load.lisp")
 (load "../07.lisp")
 
+
+(setf win (make-instance 'window))
+;(set-x (set-x win 500) 300)
+
+(setf pic (make-instance 'picture))
+(set-shape win pic)
+
+
 ;   _______
 ;   ___0___
 ;| |       | |
@@ -18,31 +26,23 @@
 ;| |       | |
 ;   _______
 ;   ___6___
-
-(setf win (make-instance 'window))
-
-(setf pic (make-instance 'picture))
-(set-shape win pic)
-
-
-(defvar pi2 1.57) ;pi pul, pootoceni o 90stupnu
-
 (set-items pic
 ;posunuti a pootoceni jednotlivych segmentu
 ;                   0   1   2   3   4   5   6
-(loop for dx in '(  0   0 100   0   0 100   0)
-      for dy in '(  0   0   0 100 100 100 100)
-      for fi in '(  0 pi2 pi2   0 pi2 pi2   0)
+(loop with pi2 = (/ pi 2)
+      for dx in '(     0   0  80   0   0  80   0)
+      for dy in '(     0   0   0  80  80  80  160)
+      for fi in (list  0 pi2 pi2   0 pi2 pi2   0)
   collect
-(move
-(set-items 
-  (make-instance 'polygon)
-  (loop for x in '( 0 10 90 100 90 10  0);souradnice segmentu
-        for y in '(10  0  0  10 20 20 10)
-        collect (set-x (set-y (make-instance 'point) y) x))
-)
-dx dy)
-))
+  (let ((points (loop for x in '(10 20 80 90 80 20);souradnice segmentu
+		      for y in '(10  0  0 10 20 20)
+		  collect (set-x (set-y (make-instance 'point) y) x))))
+	(move
+        (rotate
+	(set-items (make-instance 'polygon) points)
+	 fi (car points))
+         dx dy)
+)))
 
 (redraw win)
 #|
