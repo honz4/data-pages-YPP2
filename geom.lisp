11fb1debf01e1141e822b298d0d0da1ff734f5ef
Jan Vlcek <j4n.v1cek@gmail.com>
2014-08-13 01:38:00 +0200

    priklady/testy na geometrii, LOOP makro

---
+++ geom.lisp
@@ -0,0 +1,24 @@
+#|
+exec sbcl --noinform --load $0 --end-toplevel-options "$@"
+|#
+;priklady/testy na geometrii, LOOP makro...
+;list je sudy seznam x y ... (pro polynom), vraci posunuto o x0 y0
+(defun xy-move (list x0 y0)
+  (loop with i = 1
+        for x in list
+        collect (if (oddp i) (+ x x0) (+ x y0))
+        do (incf i)
+        )
+  )
+
+;spocita n bodu na kruznici o polomeru r, phi0 = pi/2?
+(defun xy-body-na-kruznici (n r phi0)
+  (loop with phi = phi0 and step = (/ (* 2 pi) n)
+        for i from 0 below n
+        append (list (* r (cos phi)) (* r (sin phi)))
+        do (incf phi step)
+        )
+  )
+
+(format t "~A~%" (xy-move '(1 2 3 4) 10 100))
+(format t "~A~%" (xy-body-na-kruznici 4 10 0))
